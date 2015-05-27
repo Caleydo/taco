f308e64c4a1e29b6acc48a21b2687a4a28b7fb31
Reem <real_reem@hotmail.com>
2015-05-27 14:29:38 +0200

    Initial testing with Caleydo
    just playing around

---
+++ heatmap.js
@@ -0,0 +1,51 @@
+/**
+ * Created by Reem on 5/13/2015.
+ */
+
+/*
+ heatmap code from here http://jsfiddle.net/QWLkR/2/
+ */
+var h_data = [
+  {score: 0.5, row: 0, col: 0},
+  {score: 0.7, row: 0, col: 1},
+  {score: 0.2, row: 1, col: 0},
+  {score: 0.3, row: 1, col: 1},
+  {score: 0.1, row: 2, col: 0},
+  {score: 0.4, row: 2, col: 1},
+  {score: 0.0, row: 0, col: 2},
+  {score: -0.8, row: 1, col: 2},
+  {score: 0.6, row: 2, col: 2}
+];
+
+
+//height of each row in the heatmap
+//width of each column in the heatmap
+var gridSize = 16,
+  h = gridSize,
+  w = gridSize,
+  rectPadding = 60;
+
+var colorLow = 'green', colorMed = 'yellow', colorHigh = 'red';
+
+var margin = {top: 20, right: 80, bottom: 30, left: 50},
+  width = 640 - margin.left - margin.right,
+  height = 380 - margin.top - margin.bottom;
+
+var colorScale = d3.scale.linear()
+  .domain([-1, 0, 1])
+  .range([colorLow, colorMed, colorHigh]);
+
+var svg = d3.select("#board").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var heatMap = svg.selectAll(".board")
+  .data(h_data, function(d) { return d.col + ':' + d.row; })
+  .enter().append("svg:rect")
+  .attr("x", function(d) { return d.row * w; })
+  .attr("y", function(d) { return d.col * h; })
+  .attr("width", function(d) { return w; })
+  .attr("height", function(d) { return h; })
+  .style("fill", function(d) { return colorScale(d.score); });
---
+++ main.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Reem on 5/13/2015.
+ * based on what is
+ * Created by Samuel Gratzl on 15.12.2014.
+ */
+
+define(['../caleydo/data', 'd3', 'jquery', '../caleydo/event', '../caleydo-selectioninfo/main', '../caleydo/idtype', 'bootstrap', 'font-awesome'], function (data, d3, $, events, idtypes, links) {
+  'use strict';
+  //var info = selectionInfo.create(document.getElementById('selectioninfo'));
+  //var board = boards.create(document.getElementById('board'));
+
+  function toType(desc) {
+    if (desc.type === 'vector') {
+      return desc.value.type === 'categorical' ? 'partition' : 'numerical';
+    }
+    return desc.type;
+  }
+
+  data.list().then(function (items) {
+    items = items.filter(function (d) {
+      return d.desc.type !== 'matrix';
+    });
+    var $base = d3.select('#blockbrowser table tbody');
+    var $rows = $base.selectAll('tr').data(items);
+    $rows.enter().append('tr').html(function (d) {
+      return '<th>' + d.desc.name + '</th><td>' + toType(d.desc) + '</td><td>' +
+        d.idtypes.map(function (d) { return d.name; }).join(', ') + '</td><td>' + d.dim.join(' x ') + '</td>';
+    }).attr('draggable', true)
+      .on('dragstart', function (d) {
+        var e = d3.event;
+        e.dataTransfer.effectAllowed = 'copy'; //none, copy, copyLink, copyMove, link, linkMove, move, all
+        e.dataTransfer.setData('text/plain', d.desc.name);
+        e.dataTransfer.setData('application/json', JSON.stringify(d.desc));
+        var p = JSON.stringify(d.persist());
+        e.dataTransfer.setData('application/caleydo-data-item', p);
+        //encode the id in the mime type
+        e.dataTransfer.setData('application/caleydo-data-item-' + p, p);
+        //board.currentlyDragged = d;
+      });
+  });
+});
