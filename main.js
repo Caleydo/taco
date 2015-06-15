bcd4d5143b8618d36c316c6571016131f67e45ea
Reem Hourieh <rhourieh@gmail.com>
2015-06-15 20:17:58 +0200

    Created difflog_parser module to parse the difflog tsv file
    Just console log the content which is an object with arrays for each
    added/deleted row/column ids

--- main.js
+++ main.js
@@ -4,9 +4,12 @@
  * Created by Samuel Gratzl on 15.12.2014.
  */
 
-require(['../caleydo/data', 'd3', 'jquery', '../caleydo/event', '../caleydo/idtype', '../caleydo-window/main', '../caleydo/main', '../caleydo/multiform', 'bootstrap', 'font-awesome'], function (data, d3, $, events, idtypes, window, C, multiform) {
+require(['../caleydo/data', 'd3', 'jquery', './difflog_parser', 'bootstrap', 'font-awesome'],
+  function (data, d3, $, difflog_parser) {
   'use strict';
   var windows = $('<div>').css('position', 'absolute').appendTo('#main')[0];
+
+    var diff_arrays = difflog_parser.Diff;
 
   function toType(desc) {
     if (desc.type === 'vector') {
@@ -43,39 +46,4 @@ require(['../caleydo/data', 'd3', 'jquery', '../caleydo/event', '../caleydo/idty
     });
   });
 
-  var filepath = 'data/tiny_table.log';
-
-  var added_rows, deleted_rows, added_cols, deleted_cols = [];
-
-  var rows = d3.tsv(filepath, function (dataset) {
-    dataset.forEach(function(d) {
-      return {
-       operation: d.operation,
-       type: d.type,
-       id: d.id, // TODO: consider that the change operation returns 2 values here
-       position: d.position // convert to number use: +d.position
-       };
-    });
-  });
-
- console.log(rows);
-  /*
-  if (d.operation == "add") {
-    if (d.type == 'column') {
-      added_cols.push(d.id);
-      console.log("1");
-    } else if (d.type == 'row') {
-      added_rows.push(d.id);
-      console.log("2");
-    }
-  } else if (d.operation == "delete") {
-    if (d.type == 'column') {
-      deleted_cols.push(d.id);
-      console.log("3");
-    } else if (d.type == 'row') {
-      deleted_rows.push(d.id);
-      console.log("4");
-    }
-  }*/
-  //console.log(added_cols, added_rows, deleted_cols, deleted_rows);
 });
