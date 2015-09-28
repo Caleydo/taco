317b8610be31fe1bd8e80fd74885d85ba5d20a32
Reem Hourieh <rhourieh@gmail.com>
2015-09-28 17:09:17 +0200

    minimizing code repetition for drawing diff plot for content and structure changes

--- diff_barplot.js
+++ diff_barplot.js
@@ -3,55 +3,25 @@
  */
 define(['exports', 'd3', '../caleydo_d3/d3util', './drag'], function (exports, d3, d3utils, drag) {
     //draws the barplot based on the projected data
-    function drawDiffBarplot(p_data, usize, parent, dim, realsize, data_promise) {
-      var usize0 = usize[0],
-        usize1 = usize[1],
-        is_cols = false;
-      if (dim[0] !== "rows"){
-        usize0 = usize[1];
-        usize1 = usize[0];
-        is_cols = true;
-      }
-      var  gridSize = 6,
-        //todo we could use the width of the max value
-        width = (realsize ?(usize1 * gridSize) : 20),
-        height = (usize0 * gridSize);
-      var myDrag = drag.Drag();
+    function drawDiffBarplot(p_data, usize1, width, gridSize, parent, y, realsize) {
 
-      console.log("direction", dim, data_promise);
-
-      //find a better way for calculating the position
-      var position = parseInt(parseInt(d3.select("#board").style("width")) / 2) - parseInt(width / 2);
-
-      var container = parent.classed("rotated", is_cols)
-        .style("width", width + 2 + 'px')
-        .style("height", height + 2 + 'px')
-        //todo find an alternative for margin.top here!! or in the other heatmap (special margin)
-        //todo move all the transform functions to the css
-        //note that the transform has to be one sentence otherwise it won't happen
-        .style("transform", "translate(" + position + "px," + 20 + "px)" + (is_cols ? "rotate(90deg) scaleY(-1)" : ""))
-        .call(myDrag);
-
-      var max_change = Math.max.apply(Math, p_data.map(function(o){return o.count;}));
+      var max_change = Math.max.apply(Math, p_data.map(function (o) {
+        return o.count;
+      }));
 
       //http://bost.ocks.org/mike/bar/
       var x;
-      if (realsize){
+      if (realsize) {
         x = d3.scale.linear()
-        .domain([0, usize1])
-        .range([0, width]);
-      }else{
+          .domain([0, usize1])
+          .range([0, width]);
+      } else {
         x = d3.scale.linear()
-        .domain([0, max_change])
-        .range([0, width]);
+          .domain([0, max_change])
+          .range([0, width]);
       }
 
-      var y = d3.scale.linear()
-        .domain([0, usize0])
-        .range([0, height]);
-
-
-      var bp = container.selectAll("div.rows")
+      var bp = parent.selectAll("div.rows")
         .data(p_data, function (d, i) {
           return d.id;
         });
@@ -62,77 +32,80 @@ define(['exports', 'd3', '../caleydo_d3/d3util', './drag'], function (exports, d
         .style("width", function (d) {
           return x(d.count) + "px";
         })
-        .style("height", gridSize -1 + "px")
-        .attr("title", function (d) {return d.count;})
+        .style("height", gridSize - 1 + "px")
+        .attr("title", function (d) {
+          return d.count;
+        })
         //.text(function (d) {return d.id;})
-        .style("transform", function(d){ return "translate(" + 0 + "px," + y(d.pos) + "px)";});
-      return container; //?
+        .style("transform", function (d) {
+          return "translate(" + 0 + "px," + y(d.pos) + "px)";
+        });
+      return parent;
     }
 
-    function drawDiffStructPlot (p_data, usize, parent, dim, realsize, isadd) {
-      var usize0 = usize[0],
-        usize1 = usize[1],
-        is_cols = false;
-      if (dim[0] !== "rows"){
-        usize0 = usize[1];
-        usize1 = usize[0];
-        is_cols = true;
-      }
-      var  gridSize = 6,
-        //todo we could use the width of the max value
-        width = (realsize ?(usize1 * gridSize) : 20),
-        height = (usize0 * gridSize);
-
-      var y = d3.scale.linear()
-        .domain([0, usize0])
-        .range([0, height]);
-
-      //todo this is only needed if the content is not applied!!!!
-      var myDrag = drag.Drag();
-      //find a better way for calculating the position
-      var position = parseInt(parseInt(d3.select("#board").style("width")) / 2) - parseInt(width / 2);
-      var container = parent.classed("rotated", is_cols)
-        .style("width", width + 2 + 'px')
-        .style("height", height + 2 + 'px')
-        //todo find an alternative for margin.top here!! or in the other heatmap (special margin)
-        //todo move all the transform functions to the css
-        //note that the transform has to be one sentence otherwise it won't happen
-        .style("transform", "translate(" + position + "px," + 20 + "px)" + (is_cols ? "rotate(90deg) scaleY(-1)" : ""))
-        .call(myDrag);
-
-      container = parent.selectAll("div.struct")
+    function drawDiffStructPlot(p_data, gridSize, parent, y, isadd) {
+      var container = parent.selectAll("div.struct")
         .data(p_data, function (d, i) {
-          console.log("the data is" , d);
           return d.id;
         });
 
-        container.enter().append("div")
+      container.enter().append("div")
         .classed("struct", true)
         .classed("struct-add-color", isadd)
         .classed("struct-del-color", !isadd)
-        .style("width", gridSize -1 + "px")
-        .style("height", gridSize -1 + "px")
+        .style("width", gridSize - 1 + "px")
+        .style("height", gridSize - 1 + "px")
         //.attr("title", function (d) {return d.id;})
-        .style("transform", function(d){ return "translate(" + -parseInt(gridSize/2) + "px," + y(d.pos) + "px)";});
+        .style("transform", function (d) {
+          return "translate(" + -parseInt(gridSize / 2) + "px," + y(d.pos) + "px)";
+        });
       return parent;
     }
 
     exports.DiffBarPlotVis = d3utils.defineVis('DiffBarPlotVis', {
-        dim: 'column'
+        dim: ['column']
       }, [200, 200],
       function ($parent, data, size) {
         var o = this.options;
-        //var $node = $parent.append('pre');
+        var usize0 = data.desc.size[0],
+          usize1 = data.desc.size[1],
+          is_cols = false;
+        //todo conside the direction in a better way
+        if (data.desc.direction[0] !== "rows") {
+          usize0 = data.desc.size[1];
+          usize1 = data.desc.size[0];
+          is_cols = true;
+        }
+        var realsize = false,
+          gridSize = 6,
+        //todo we could use the width of the max value
+          width = (realsize ? (usize1 * gridSize) : 20),
+          height = (usize0 * gridSize);
+        var myDrag = drag.Drag();
+
+        var y = d3.scale.linear()
+          .domain([0, usize0])
+          .range([0, height]);
+
+        //todo find a better way for calculating the position
+        var position = parseInt(parseInt(d3.select("#board").style("width")) / 2) - parseInt(width / 2);
+
         var $node = $parent.append("div")
-        .classed("taco-bp-container", true);
+          .classed("taco-bp-container", true)
+          .classed("rotated", is_cols)
+          .style("width", width + 2 + 'px')
+          .style("height", height + 2 + 'px')
+          //todo find an alternative for margin.top here!! or in the other heatmap (special margin)
+          //todo move all the transform functions to the css
+          //note that the transform has to be one sentence otherwise it won't happen
+          .style("transform", "translate(" + position + "px," + 20 + "px)" + (is_cols ? "rotate(90deg) scaleY(-1)" : ""))
+          .call(myDrag);
 
-        var realsize = false;
-
-        //todo change this so that it consider the case of both rows and cols at the same time
         if (data.desc.change.indexOf('content') > -1) {
+          //todo change this so that it consider the case of both rows and cols at the same time
           data.dimStats(data.desc.direction[0]).then(function (stats) {
             //$node.text(JSON.stringify(stats, null, ' '));
-            $node = drawDiffBarplot(stats, data.desc.size, $node, data.desc.direction, realsize, data.data());
+            $node = drawDiffBarplot(stats, usize1, width, gridSize, $node, y, realsize);
           });
         }
         //todo combine it with the one above
@@ -142,9 +115,9 @@ define(['exports', 'd3', '../caleydo_d3/d3util', './drag'], function (exports, d
               console.log("values", values);
               var a_stats = values[0],
                 d_stats = values[1];
-              $node = drawDiffStructPlot(a_stats, data.desc.size, $node, data.desc.direction, realsize, true);
+              $node = drawDiffStructPlot(a_stats, gridSize, $node, y, true);
               //todo fix the bug with the container
-              $node = drawDiffStructPlot(d_stats, data.desc.size, $node, data.desc.direction, realsize, false);
+              $node = drawDiffStructPlot(d_stats, gridSize, $node, y, false);
             });
         }
         return $node;
