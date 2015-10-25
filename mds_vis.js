f1120aea468ef526291137a7286490dadcc56643
Reem Hourieh <rhourieh@gmail.com>
2015-10-25 22:13:44 +0100

    added force directed example

--- mds_vis.js
+++ mds_vis.js
@@ -159,10 +159,82 @@ define(['exports', 'd3', '../caleydo_d3/d3util', './drag'], function (exports, d
         .attr("height", "100%");
       d3scatterplot(svg, data.X, data.D, data.cities);
 
-
       return svg;
     }
 
+    //end of mds part
+    //beginning of force directed graph
+    function drawFDGraph(parent, mdata, size){
+      //todo use size instead
+      var width = 500,
+        height = 500;
+      var svg = parent.append("svg")
+        .attr("width", width)
+        .attr("height", height);
+      drawForceGraph(svg, mdata, width, height);
+      return svg;
+    }
+
+    function drawForceGraph(svg, graph, width, height){
+      var color = d3.scale.category20();
+
+      var force = d3.layout.force()
+        .charge(-120)
+        .linkDistance(30)
+        .size([width, height]);
+
+      force
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .start();
+
+      var link = svg.selectAll(".link")
+        .data(graph.links)
+        .enter().append("line")
+        .attr("class", "link")
+        .style("stroke-width", function (d) {
+          return Math.sqrt(d.value);
+        });
+
+      var node = svg.selectAll(".node")
+        .data(graph.nodes)
+        .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", 5)
+        .style("fill", function (d) {
+          return color(d.group);
+        })
+        .call(force.drag);
+
+      node.append("title")
+        .text(function (d) {
+          return d.name;
+        });
+
+      force.on("tick", function () {
+        link.attr("x1", function (d) {
+          return d.source.x;
+        })
+          .attr("y1", function (d) {
+            return d.source.y;
+          })
+          .attr("x2", function (d) {
+            return d.target.x;
+          })
+          .attr("y2", function (d) {
+            return d.target.y;
+          });
+
+        node.attr("cx", function (d) {
+          return d.x;
+        })
+          .attr("cy", function (d) {
+            return d.y;
+          });
+      });
+    }
+
+    //end of fd graph
     exports.MDSVis = d3utils.defineVis('DiffBarPlotVis', {
         dim: ['column']
       }, [200, 200],
@@ -171,7 +243,8 @@ define(['exports', 'd3', '../caleydo_d3/d3util', './drag'], function (exports, d
         var $node = $parent.append("div")
           .classed("d3-scatter-output", true);
         data.data().then(function(mdata){
-          drawMDSGraph($parent, mdata, size);
+          //drawMDSGraph($parent, mdata, size);
+          drawFDGraph($parent, mdata, size);
         });
         return $node;
       });
