001204fea2a9e1da60ae0961731bacd82bc2d0c0
Holger Stitz <holger.stitz@jku.at>
2016-08-25 18:09:44 +0200

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Holger Stitz on 25.08.2016.
- */
-define(["require", "exports", './Taco', "css!/bower_components/bootstrap/dist/css/bootstrap", "font-awesome", "css!../caleydo_bootstrap_fontawesome/style.css"], function (require, exports, taco) {
-    "use strict";
-    var parent = document.querySelector('#taCoApp');
-    taco.create(parent);
-});
-//# sourceMappingURL=main.js.map
