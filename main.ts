d6d0d6bdea9702a363c86e8e76ec320d6225ea42
Holger Stitz <holger.stitz@jku.at>
2016-08-25 18:17:22 +0200

    Use bootstrap.min.css

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@
 // Determine the order of css files manually
 
 // HACK! because <amd-dependency path="bootstrap" /> is loaded after all the other stylesheets and not before (as declared)
-/// <amd-dependency path="css!/bower_components/bootstrap/dist/css/bootstrap" />
+/// <amd-dependency path="css!/bower_components/bootstrap/dist/css/bootstrap.min" />
 
 /// <amd-dependency path="font-awesome" />
 /// <amd-dependency path="css!../caleydo_bootstrap_fontawesome/style.css" />
