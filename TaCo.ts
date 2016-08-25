30839e4733b4fb0eace7e144153c6df818ae0387
Holger Stitz <holger.stitz@jku.at>
2016-08-25 18:08:27 +0200

    Started basic implementation of TaCo 2 and moved old files into v1 directory

---
+++ TaCo.ts
@@ -0,0 +1,29 @@
+/**
+ * Created by Holger Stitz on 25.08.2016.
+ */
+
+import d3 = require('d3');
+
+/**
+ * The main class for the TaCo app
+ */
+export class TaCo {
+
+  private $node;
+
+  constructor(parent:Element) {
+    this.$node = d3.select(parent);
+
+    this.$node.html(`<h1 style="margin: 20px;">Welcome to TaCo 2</h1>`);
+  }
+
+}
+
+/**
+ * Factory method to create a new TaCo instance
+ * @param parent
+ * @returns {TaCo}
+ */
+export function create(parent:Element) {
+  return new TaCo(parent);
+}
---
+++ main.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by Holger Stitz on 25.08.2016.
+ */
+
+// Determine the order of css files manually
+
+// HACK! because <amd-dependency path="bootstrap" /> is loaded after all the other stylesheets and not before (as declared)
+/// <amd-dependency path="css!/bower_components/bootstrap/dist/css/bootstrap" />
+
+/// <amd-dependency path="font-awesome" />
+/// <amd-dependency path="css!../caleydo_bootstrap_fontawesome/style.css" />
+
+
+import taco = require('./Taco');
+
+const parent = document.querySelector('#taCoApp');
+
+taco.create(parent);
