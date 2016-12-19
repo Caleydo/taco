4a70348ccc4e4673da5ef598798438e0814fbc67
NChristina <Christina.Niederer@fhstp.ac.at>
2016-12-19 12:39:56 +0100

    Refactor Code Group changes, Filter changes - work in progress #23 #28 #22

--- src/timeline.ts
+++ src/timeline.ts
@@ -69,17 +69,17 @@ class Timeline implements IAppView {
    */
   private build() {
 
-      this.$navbar.html(` <div id="nav-bar">  
-      <div class="btn-group" role="group" aria-label="...">
-         <button type="button" class="btn btn-default" id="btn-nochange">No changes</button>
-         <button type="button" class="btn btn-default" id="btn-removed">Removed</button>
-         <button type="button" class="btn btn-default" id="btn-added">Added</button>
-         <button type="button" class="btn btn-default" id="btn-content">Content</button>        
+    this.$navbar.html(` <div id="nav-bar">  
+      <div class="btn-group change" role="group" aria-label="...">
+         <button type="button" class="btn btn-default" id="btn-nochange " data-change-type="nochanges">No changes</button>
+         <button type="button" class="btn btn-default" id="btn-removed" data-change-type="removed">Removed</button>
+         <button type="button" class="btn btn-default" id="btn-added" data-change-type="added">Added</button>
+         <button type="button" class="btn btn-default" id="btn-content" data-change-type="content">Content</button>        
       </div>  
          
-      <div class="btn-group" role="group" aria-label="...">
-        <button type="button" class="btn btn-default toggleTimeline" id="btn-timeline">Show/Hide Timeline</button> 
-        <button type="button" class="btn btn-default" id="btn-group">Group Changes</button> 
+      <div class="btn-group filter" role="group" aria-label="...">
+        <button type="button" class="btn btn-default toggleTimeline" id="btn-timeline" data-change-type="timefilter">Show/Hide Timeline</button> 
+        <button type="button" class="btn btn-default toggleGroup" id="btn-group">Group Changes</button> 
         <button type="button" class="btn btn-default" id="btn-stacked">Show as stacked bars</button> 
      </div>
      </div>`);
@@ -88,6 +88,7 @@ class Timeline implements IAppView {
     // TODO build timeline using D3 of parts that doesn't change on update()
     this.$node.html(`           
       <div id="timeline"></div>
+      <div id="groupedBars"></div>  
     `);
 
 
@@ -96,7 +97,7 @@ class Timeline implements IAppView {
       .attr('width', this.timelineWidth)
       .attr('height', this.timelineHeight);
 
-    console.log(d3.select('.selector').selectAll('div'));
+    // console.log(d3.select('.selector').selectAll('div'));
 
   }
 
@@ -191,11 +192,15 @@ class Timeline implements IAppView {
 
     this.drawTimeline();
 
+    // console.log('toggleTimeline', this.$navbar.select('div .btn-group.filter').select('.toggleTimeline'));
     // get toogle Button
-    let showHideButton = this.$navbar.select('.toggleTimeline');
+    let showHideButton = this.$navbar.select('div .btn-group.filter').select('.toggleTimeline');
+    // console.log('show/hide button', showHideButton);
 
     // Hide and Show timeline (line + circles)
     showHideButton.on('click', function (e) {
+      // let button = d3.select(this);
+      //console.log(button);
       let line = that.$svgTimeline.select('line');
       let circle = that.$svgTimeline.selectAll('circle');
       //console.log(line, circle);
@@ -203,21 +208,211 @@ class Timeline implements IAppView {
       if (line.size() > 0 && circle.size() > 0) {
         line.remove();
         circle.remove();
-       // console.log(line.size(), circle.size());
+        // console.log(line.size(), circle.size());
       } else {
         //console.log(line.size(), circle.size());
         that.drawTimeline();
       }
     });
-
     // Create Bars
     const barPromises = this.generateBars(20);
+
 
     // Check if all bars have been loaded
     Promise.all(barPromises).then((bars) => {
       console.log('finished loading of all bars');
     });
+
+
+    // creating 2D Ratio bars
+    let noratio = [];
+    let aratio = [];
+    let cratio = [];
+    let dratio = [];
+    let finalArray:any = [];
+
+    function requestDataGroupedBars() {
+      const ids = items.map((d) => d.item.desc.id);
+
+      //console.log(ids);
+
+      return d3.pairs(ids).map((pair) => {
+        return Promise.all([ajax.getAPIJSON(`/taco/diff_log/${pair[0]}/${pair[1]}/1/1/2/structure,content`), pair])
+          .then((args) => {
+            const json = args[0];
+            noratio.push(json.no_ratio);
+            aratio.push(json.a_ratio);
+            cratio.push(json.c_ratio);
+            dratio.push(json.d_ratio);
+
+          });
+      });
+    }
+
+    const groupedBars = d3.selectAll('#groupedBars').append('svg');
+
+    function openGroupedBars() {
+      Promise.all(requestDataGroupedBars()).then((bars) => {
+        finalArray = noratio.concat(aratio, cratio, dratio);
+        drawgroupedBars(finalArray);
+      });
+    }
+
+    // openGroupedBars();
+    let buttonGroup = this.$navbar.select('div .btn-group.filter').select('.toggleGroup');
+    console.log('buttonGroup', buttonGroup);
+
+    buttonGroup.on('click', function (e) {
+      // let line =  this.$svgTimeline.select('line');
+      //let circle =  this.$svgTimeline.selectAll('circle');
+      let bars =  that.$svgTimeline.selectAll('rect');
+      // let line = that.$svgTimeline.select('line');
+      //let circle = that.$svgTimeline.selectAll('circle');
+
+
+
+      bars.remove();
+
+      //if (line.size() > 0 && circle.size() > 0 && bars.size() > 0) {
+      if ( bars.size() > 0) {
+        //  line.remove();
+        //circle.remove();
+        bars.remove();
+        openGroupedBars();
+      } else {
+        groupedBars.remove();
+        that.generateBars(20);
+
+      }
+    });//End button remove
+
+    //openGroupedBars();
+
+
+    function drawgroupedBars(finalArray) {
+      const w = 300;
+      const h = 80;
+
+      groupedBars.selectAll('rect')
+        .data(finalArray)
+        .enter()
+        .append('rect')
+        //.attr('x', (d, i) => i * (w / noratio.length - barPadding) + 20)
+        .attr('x', (d, i) => i * (w / 20))
+        .attr('y', (d:any, i) => h - (d * 100))
+        .attr('width', 10 + 'px')
+        .attr('height', (d:any) => d * 100);
+      /*.attr('fill', function (d, i) {
+
+       if (i <= 5) {
+       return '#D8D8D8';
+       } else if (i > 5) {
+       return '#67c4a7';
+       } else if (i > 12) {
+       return '#8DA1CD';
+       }
+       });*/
+    }
+
+
+      //helper variable for clicking remove button!
+          let isbtnClicked = false;
+
+          let changeButton = this.$navbar.select('div .btn-group.change');
+          //this.$navbar.selectAll('div .btn-group.change').on('click', function () {
+          changeButton.on('click', function () {
+
+            //which button is clicked
+            const button = d3.select(this);
+            //console.log('button', button);
+            // which change type
+            const changeType =  button.attr('data-change-type');
+            //console.log('changeType', changeType);
+            //color and data for drawing bars which all change types
+            let colors =  that.color;
+            let datas = that.data_all;
+
+            //remove all currents bars
+            that.$svgTimeline.selectAll('g').remove();
+
+            // show default all bars and colors
+            if(isbtnClicked === false) {
+              // console.log('Button is clickt');
+             // drawBars(datas, colors);
+              isbtnClicked  = false;
+
+            }
+            //console.log('Übergabeparameter', changeType);
+            //override colors for change type
+            switch (changeType) {
+              case 'nochanges':
+                colors =  that.color_nochanges;
+                //datas = data_nochange;
+                //console.log('No change');
+                // console.log(colors, datas);
+                break;
+              case 'removed':
+                colors =  that.color_removed;
+                //datas = data_removed;
+                break;
+            }
+            //console.log('current Data and color', datas, colors);
+            //console.log(changeType, datas, colors);
+
+            that.generateBars(20);
+            // isbtnClicked = true;
+
+          });
+
+
+    /*console.log('request Data for changeBars', this.requestData());
+     Promise.all(this.requestData()).then((bars) => {
+        console.log('get Data');
+      });*/
+
+   //console.log(this.requestData());
+
   }
+
+  private color = d3.scale.ordinal()
+    .range(['#D8D8D8', '#67c4a7', '#8DA1CD', '#F08E65']);
+
+  private color_removed = d3.scale.ordinal()
+    .range(['#D8D8D8', '#67c4a7', '#8DA1CD']);
+
+  private color_added = d3.scale.ordinal()
+    .range(['#D8D8D8', '#8DA1CD', '#F08E65']);
+
+  private color_content = d3.scale.ordinal()
+    .range(['#D8D8D8', '#67c4a7', '#F08E65']);
+
+  private color_nochanges = d3.scale.ordinal()
+    .range(['#67c4a7', '#8DA1CD', '#F08E65']);
+
+  private data_all =[];
+
+
+  /*private requestData () {
+    const ids = this.items.map((d) => d.item.desc.id);
+
+    //console.log(ids);
+
+    return d3.pairs(ids).map((pair) => {
+      return Promise.all([ajax.getAPIJSON(`/taco/diff_log/${pair[0]}/${pair[1]}/1/1/2/structure,content`), pair])
+        .then((args) => {
+          const json = args[0];
+          const pair = args[1];
+
+          const pairPosX = pair.map((d) => parseFloat(d3.select(`#circle_${d}`).attr('cx')));
+
+          //console.log(json, pair, pairPosX);
+
+          return json;
+
+
+        });
+    });
+  }*/
 
   // creating 2D Ratio bars
   private generateBars(width) {
@@ -233,10 +428,6 @@ class Timeline implements IAppView {
           const json = args[0];
           const pair = args[1];
 
-          //console.log(json, pair);
-
-         // console.log(d3.select('circle'));
-
           const pairPosX = pair.map((d) => parseFloat(d3.select(`#circle_${d}`).attr('cx')));
 
           console.log('finished loading pair - BARS', pair, pairPosX, json);
@@ -245,37 +436,111 @@ class Timeline implements IAppView {
           const h = 30;
           const barPadding = 0.5;
 
-          const data = [json.no_ratio, json.a_ratio, json.c_ratio, json.d_ratio];
+          //const data_all = [json.no_ratio, json.a_ratio, json.c_ratio, json.d_ratio];
+          this.data_all = [json.no_ratio, json.a_ratio, json.c_ratio, json.d_ratio];
 
-          const color = d3.scale.ordinal()
-            .domain(<any>[0, data.length - 1])
-            .range(['#D8D8D8', '#67C4A7', '#8DA1CD', '#F08E65']);
+          const data_removed = [json.no_ratio, json.a_ratio, json.c_ratio];
+
+          const data_content = [json.no_ratio, json.a_ratio, json.d_ratio];
+
+          const data_added = [json.no_ratio, json.c_ratio, json.d_ratio];
+
+          const data_nochange = [json.a_ratio, json.c_ratio, json.d_ratio];
+
+         /* const svgRatioBar = this.$svgTimeline.append('g')
+           .style('transform', 'translate(' + (pairPosX[0] + 0.5 * (pairPosX[1] - pairPosX[0] - width)) + 'px)');*/
+
+          //all changes
+          //drawBars(this.data_all, that.color);
 
 
-          const svgRatioBar = this.$svgTimeline.append('g')
-            .style('transform', 'translate(' + (pairPosX[0] + 0.5 * (pairPosX[1] - pairPosX[0] - width)) + 'px)');
+         // function drawBars(data:any, color) {
 
-          svgRatioBar.selectAll('rect')
-            .data(data)
-            .enter()
-            .append('rect')
-            .attr('x', (d, i) => i * (w / data.length - barPadding))
-            .attr('y', (d, i) => h - d * 100)
-            .attr('width', width)
-            .attr('height', (d) => d * 100)
-            .attr('fill', (d, i) => <string>color(i.toString()))
-            .on('click', function () {
-              const currentPosX = d3.transform(d3.select(this.parentNode).style('transform')).translate[0];
+            const svgRatioBar = that.$svgTimeline.append('g')
+              .style('transform', 'translate(' + (pairPosX[0] + 0.5 * (pairPosX[1] - pairPosX[0] - width)) + 'px)');
 
-              if (that.open2dHistogram === this.parentNode) {
-                events.fire(AppConstants.EVENT_CLOSE_2D_HISTOGRAM);
-                that.open2dHistogram = null;
+            console.log('g Element', svgRatioBar, pairPosX);
 
-              } else {
-                events.fire(AppConstants.EVENT_OPEN_2D_HISTOGRAM, currentPosX, pair);
-                that.open2dHistogram = this.parentNode;
-              }
-            });
+            svgRatioBar.selectAll('rect')
+              .data(this.data_all)
+              .enter()
+              .append('rect')
+              .attr('x', (d, i) => i * (w / this.data_all.length - barPadding))
+              .attr('y', (d, i) => h - d * 100)
+              .attr('width', width)
+              .attr('height', (d) => d * 100)
+              .attr('fill', (d, i) => <string>this.color(i.toString()))
+              //.attr('fill', (d, i) => testcolor)
+              //.attr('fill', (d, i) => <string>color[i])
+              .on('click', function () {
+                const currentPosX = d3.transform(d3.select(this.parentNode).style('transform')).translate[0];
+
+                if (that.open2dHistogram === this.parentNode) {
+                  events.fire(AppConstants.EVENT_CLOSE_2D_HISTOGRAM);
+                  that.open2dHistogram = null;
+
+                } else {
+                  events.fire(AppConstants.EVENT_OPEN_2D_HISTOGRAM, currentPosX, pair);
+                  that.open2dHistogram = this.parentNode;
+                }
+              });
+         // }
+
+
+
+        /*  //helper variable for clicking remove button!
+          let isbtnClicked = false;
+
+          let changeButton = this.$navbar.select('div .btn-group.change');
+          //this.$navbar.selectAll('div .btn-group.change').on('click', function () {
+          changeButton.on('click', function () {
+
+            //which button is clicked
+            const button = d3.select(this);
+            //console.log('button', button);
+            // which change type
+            const changeType =  button.attr('data-change-type');
+            //console.log('changeType', changeType);
+            //color and data for drawing bars which all change types
+            let colors =  that.color;
+            let datas = data_all;
+
+            //remove all currents bars
+            that.$svgTimeline.selectAll('g').remove();
+
+            // show default all bars and colors
+            if(isbtnClicked === false) {
+              // console.log('Button is clickt');
+              drawBars(datas, colors);
+              isbtnClicked  = false;
+
+            }
+            //console.log('Übergabeparameter', changeType);
+            //override colors for change type
+            switch (changeType) {
+              case 'nochanges':
+                colors =  that.color_nochanges;
+                datas = data_nochange;
+                //console.log('No change');
+                // console.log(colors, datas);
+                break;
+              case 'removed':
+                colors =  that.color_removed;
+                datas = data_removed;
+                break;
+            }
+            //console.log('current Data and color', datas, colors);
+            //console.log(changeType, datas, colors);
+
+            drawBars(datas, colors);
+            // isbtnClicked = true;
+
+          });
+*/
+
+
+
+
 
         });
     });
