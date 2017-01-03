3134246c58297e11a4fdec282709911115007e88
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-01-03 14:38:05 +0100

    fix renaming in code

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@
 import 'file-loader?name=index.html!extract-loader!html-loader!./index.html';
 import 'file-loader?name=404.html!./404.html';
 import 'file-loader?name=robots.txt!./robots.txt';
-import 'phovea_bootstrap_fontawesome/src/_bootstrap';
-import 'phovea_bootstrap_fontawesome/src/_font-awesome';
+import 'phovea_ui/src/_bootstrap';
+import 'phovea_ui/src/_font-awesome';
 import './style.scss';
 
 import * as app from './app';
-import * as header from 'phovea_bootstrap_fontawesome/src/header';
+import * as header from 'phovea_ui/src/header';
 import {Language} from './language';
 
 header.create(
