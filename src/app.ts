150baea98ffef56b6379e6722d29f9cd5444495d
Holger Stitz <holger.stitz@jku.at>
2017-03-14 13:07:46 +0100

    Remove .invisibleClass2

--- src/app.ts
+++ src/app.ts
@@ -153,8 +153,8 @@ export class App implements IAppView {
       .then((viewInstances) => {
         // loading and initialization has finished -> hide loading indicator
         this.setBusy(false);
-        d3.select('.placeholderContainer').classed('invisibleClass2', false);
-        d3.select('.detailview').classed('invisibleClass2', false);
+        d3.select('.placeholderContainer').classed('hidden', false);
+        d3.select('.detailview').classed('hidden', false);
         return this;
       });
 
