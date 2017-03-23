eda2c20aeeba4f38e63bc67b99e0c04b89bf42b1
Holger Stitz <holger.stitz@jku.at>
2017-03-23 15:02:00 +0100

    Refactored diff_heat_map

--- src/diff_heat_map.ts
+++ src/diff_heat_map.ts
@@ -7,6 +7,7 @@ import * as d3 from 'd3';
 import * as events from 'phovea_core/src/event';
 import * as ajax from 'phovea_core/src/ajax';
 import {AppConstants, IChangeType, ChangeTypes} from './app_constants';
+import {get} from 'phovea_core/src/plugin';
 
 
 /**
@@ -17,9 +18,7 @@ class DiffHeatMap implements IAppView {
   //main div
   private $node;
 
-  private items;
-
-  private selectedTables = [];
+  private initialSize = AppConstants.HEATMAP_CELL_SIZE;
 
   private colorLow = '#d8b365';
   private colorMed = 'white';
@@ -42,213 +41,179 @@ class DiffHeatMap implements IAppView {
    * @returns {Promise<HeatMap>}
    */
   init() {
+    this.build();
     this.attachListener();
     // return the promise directly as long there is no dynamical data to update
     return Promise.resolve(this);
   }
 
-
+  private build() {
+    // wrap view ids from package.json as plugin and load the necessary files
+    get(AppConstants.VIEW, 'ReorderView')
+      .load()
+      .then((plugin) => {
+        const view = plugin.factory(
+          this.$node.node(), // parent node
+          {} // options
+        );
+        return view.init();
+      });
+  }
 
   /**
    * Attach event handler for broadcasted events
    */
   private attachListener() {
     //attach event listener
-    events.on(AppConstants.EVENT_DATA_COLLECTION_SELECTED, (evt, items) => this.updateItems(items));
-
     events.on(AppConstants.EVENT_OPEN_DIFF_HEATMAP, (evt, items) => {
-      //console.log('übergebene items', items[0], items[1]);
-      this.selectedTables = items;
-      //console.log('selected Tables', this.selectedTables);
+      if(items.length !== 2) {
+        return;
+      }
+
       this.$node.selectAll('div').remove();
-      this.diffHeatmap();
-      //console.log('selectedTables -- NOW', this.selectedTables);
+
+      const idsSelectedTable = items.map((d:any) => d.desc.id);
+      DiffHeatMap.getJSON(idsSelectedTable)
+        .then((data) => {
+          this.drawDiffHeatmap(data);
+          events.fire(AppConstants.EVENT_DIFF_HEATMAP_LOADED, items, data);
+        });
     });
 
     events.on(AppConstants.EVENT_SHOW_CHANGE, (evt, changeType: IChangeType) => this.toggleChangeType(changeType));
     events.on(AppConstants.EVENT_HIDE_CHANGE, (evt, changeType: IChangeType) => this.toggleChangeType(changeType));
-
   }
 
-   private toggleChangeType(changeType) {
+  private toggleChangeType(changeType:IChangeType) {
+    switch(changeType.type) {
+      case ChangeTypes.REMOVED.type:
+        this.$node.selectAll('.removed-color').classed('noColorClass', !changeType.isActive);
+        break;
 
-    if (changeType.type === 'removed') {
-      this.$node.selectAll('.removed-color').classed('noColorClass', !changeType.isActive);
-    }
+      case ChangeTypes.ADDED.type:
+        this.$node.selectAll('.added-color').classed('noColorClass', !changeType.isActive);
+        break;
 
-    if (changeType.type === 'added') {
-      this.$node.selectAll('.added-color').classed('noColorClass', !changeType.isActive);
-    }
-
-    if (changeType.type === 'content') {
-      this.$node.selectAll('.content-color').classed('noColorClass', !changeType.isActive);
+      case ChangeTypes.CONTENT.type:
+        this.$node.selectAll('.added-color').classed('noColorClass', !changeType.isActive);
+        break;
     }
 
     this.$node.selectAll(`div.ratio > .${changeType.type}`).classed('noColorClass', !changeType.isActive);
   }
 
-
-
-  private updateItems(items) {
-    this.items = items;
-  }
-
-  private diffHeatmap() {
-    const dataPromise = this.requestData();
-    Promise.all(dataPromise).then((data) => {
-      this.drawDiffHeatmap(data);
-    });
-    this.selectedTables = [];
-  }
-
-  private requestData() {
-    //console.log('seletedTable -- Request data', this.selectedTables);
-    return d3.pairs(this.selectedTables)
-      .map((pair) => {
-        //console.log('pair', pair);
-        // const ids = pair.map((d:any) => d.item.desc.id);
-        //console.log('ids', ids);
-        const idsSelectedTable = pair.map((d:any) => d.desc.id);
-        //console.log('ids', idsSelectedTable);
-        // return Promise.all([ajax.getAPIJSON(DiffHeatMap.getURL(ids)), pair, ids])
-        return DiffHeatMap.getJSON(idsSelectedTable)
-          .then((args) => {
-            // console.log('args', args);
-            //this.drawDiffHeatmap(args);
-            return args;
-          });
-      });
-  }
-
   private drawDiffHeatmap(data) {
-    const that = this;
     const colorScale = d3.scale.linear<string>()
       .domain([-1, 0, 1])
       .range([this.colorLow, this.colorMed, this.colorHigh])
       .clamp(true);
 
-    const diffParent = that.$node.node();
     const borderWidth = 2;
-
-    const gridHeight = diffParent.getBoundingClientRect().height - 3;
-    const gridWidth = diffParent.getBoundingClientRect().width;
-
-    const height = gridHeight - borderWidth;
-    const width = gridWidth - borderWidth;
-
-    let h = 0;
-    let w = 0;
+    const width = this.initialSize * data.union.uc_ids.length;
+    const height = this.initialSize * data.union.ur_ids.length;
 
     const root = this.$node.append('div')
       .attr('class', 'taco-table')
       .style('width', (width + borderWidth) + 'px')
       .style('height', (height + borderWidth) + 'px')
-
       .style('background-color', 'white')
       .style('transform-origin', '0 0');
 
-    //visualizing the diff
-    data.forEach(function (d) {
-      h = height / d.union.ur_ids.length;
-      //width of each column in the heatmap
-      w = width / d.union.uc_ids.length;
+    if (data.structure) {
 
-      if (d.structure) {
-
-        if(d.structure.added_rows) {
-          const addedRows = root.selectAll('.taco-added-row')
-            .data(d.structure.added_rows)
-            .enter()
-            .append('div')
-            .attr('class', 'taco-added-row')
-            .attr('class', 'added-color')
-            .attr('title', (d) => d.id)
-            .style('left', 0 + 'px')
-            .style('top', function (d) {
-              const y = d.pos;
-              return (y !== -1 ? y * h : null) + 'px';
-            })
-            .style('width', width + 'px')
-            .style('height', h + 'px');
-        }
-
-        if(d.structure.added_cols) {
-          const addedCols = root.selectAll('.taco-added-col')
-            .data(d.structure.added_cols)
-            .enter()
-            .append('div')
-            .attr('title', (d) => d.id)
-            .attr('class', 'taco-added-col')
-            .attr('class', 'added-color')
-            .style('top', 0 + 'px')
-            .style('left', (d) => {
-              const x = d.pos;
-              return (x !== -1 ? x * w : null) + 'px';
-            })
-            .style('width', w + 'px')
-            .style('height', height + 'px');
-        }
-
-        if(d.structure.deleted_rows) {
-          const deletedRows = root.selectAll('.taco-del-row')
-            .data(d.structure.deleted_rows)
-            .enter()
-            .append('div')
-            .attr('class', 'taco-del-row')
-            .attr('class', 'removed-color')
-            .attr('title', (d) => d.id)
-            .style('left', 0 + 'px')
-            .style('top', (d) => {
-              const y = d.pos;
-              return (y !== -1 ? y * h : null) + 'px';
-            })
-            .style('width', width + 'px')
-            .style('height', h + 'px');
-        }
-
-        if(d.structure.deleted_cols) {
-          const deletedCols = root.selectAll('.taco-del-col')
-            .data(d.structure.deleted_cols)
-            .enter()
-            .append('div')
-            .attr('class', 'taco-del-col')
-            .attr('class', 'removed-color')
-            .attr('title', (d) => d.id)
-            .style('top', 0 + 'px')
-            .style('left', (d) => {
-              const x = d.pos;
-              return (x !== -1 ? x * w : null) + 'px';
-            })
-            .style('width', w + 'px')
-            .style('height', height + 'px');
-        }
-      }
-
-      if (d.content) {
-        const chCells = root.selectAll('.content-color').data(d.content);
-        chCells.enter()
+      if(data.structure.added_rows) {
+        const addedRows = root.selectAll('.taco-added-row')
+          .data(data.structure.added_rows)
+          .enter()
           .append('div')
-          .attr('class', 'content-color')
-          .attr('title', (d) => {
-            return '(' + d.row + ',' + d.col + ': ' + d.diff_data + ')';
+          .attr('class', 'taco-added-row')
+          .attr('class', 'added-color')
+          .attr('title', (d) => d.id)
+          .style('left', 0 + 'px')
+          .style('top', function (d) {
+            const y = d.pos;
+            return (y !== -1 ? y * this.cellHeight : null) + 'px';
           })
-          .style('top', (d) => {
-            //var y = that.row_ids.indexOf(d.row);
-            const y = d.rpos;
-            return (y !== -1 ? y * h : null) + 'px';
-          })
-          .style('left', (d) => {
-            //var x = that.col_ids.indexOf(d.col);
-            const x = d.cpos;
-            return (x !== -1 ? x * w : null) + 'px';
-          })
-          .style('width', w + 'px')
-          .style('height', h + 'px')
-          .style('background-color', 'red')
-          .style('z-index', 1000)
-          .style('background-color', (d) => colorScale(d.diff_data));
+          .style('width', width + 'px')
+          .style('height', this.initialSize + 'px');
       }
-    });
+
+      if(data.structure.added_cols) {
+        const addedCols = root.selectAll('.taco-added-col')
+          .data(data.structure.added_cols)
+          .enter()
+          .append('div')
+          .attr('title', (d) => d.id)
+          .attr('class', 'taco-added-col')
+          .attr('class', 'added-color')
+          .style('top', 0 + 'px')
+          .style('left', (d) => {
+            const x = d.pos;
+            return (x !== -1 ? x * this.initialSize : null) + 'px';
+          })
+          .style('width', this.initialSize + 'px')
+          .style('height', height + 'px');
+      }
+
+      if(data.structure.deleted_rows) {
+        const deletedRows = root.selectAll('.taco-del-row')
+          .data(data.structure.deleted_rows)
+          .enter()
+          .append('div')
+          .attr('class', 'taco-del-row')
+          .attr('class', 'removed-color')
+          .attr('title', (d) => d.id)
+          .style('left', 0 + 'px')
+          .style('top', (d) => {
+            const y = d.pos;
+            return (y !== -1 ? y * this.initialSize : null) + 'px';
+          })
+          .style('width', width + 'px')
+          .style('height', this.initialSize + 'px');
+      }
+
+      if(data.structure.deleted_cols) {
+        const deletedCols = root.selectAll('.taco-del-col')
+          .data(data.structure.deleted_cols)
+          .enter()
+          .append('div')
+          .attr('class', 'taco-del-col')
+          .attr('class', 'removed-color')
+          .attr('title', (d) => d.id)
+          .style('top', 0 + 'px')
+          .style('left', (d) => {
+            const x = d.pos;
+            return (x !== -1 ? x * this.initialSize : null) + 'px';
+          })
+          .style('width', this.initialSize + 'px')
+          .style('height', height + 'px');
+      }
+    }
+
+    if (data.content) {
+      const chCells = root.selectAll('.content-color').data(data.content);
+      chCells.enter()
+        .append('div')
+        .attr('class', 'content-color')
+        .attr('title', (d) => {
+          return '(' + d.row + ',' + d.col + ': ' + d.diff_data + ')';
+        })
+        .style('top', (d) => {
+          //var y = that.row_ids.indexOf(d.row);
+          const y = d.rpos;
+          return (y !== -1 ? y * this.initialSize : null) + 'px';
+        })
+        .style('left', (d) => {
+          //var x = that.col_ids.indexOf(d.col);
+          const x = d.cpos;
+          return (x !== -1 ? x * this.initialSize : null) + 'px';
+        })
+        .style('width', this.initialSize + 'px')
+        .style('height', this.initialSize + 'px')
+        .style('background-color', 'red')
+        .style('z-index', 1000)
+        .style('background-color', (d) => colorScale(d.diff_data));
+    }
   }
 }
 
