301a1cfb2827c784cff2128c6a1d409b8587cd11
Holger Stitz <holger.stitz@jku.at>
2017-03-24 15:28:07 +0100

    Refactored bar_chart to ensure resizing

--- src/bar_chart.ts
+++ src/bar_chart.ts
@@ -63,21 +63,12 @@ class BarChart implements IAppView {
    * @returns {Promise<BarChart>}
    */
   init() {
-    this.build();
+    this.resize();
     this.attachListener();
 
     // Return the promise directly as long there is no dynamical data to update
     return Promise.resolve(this);
   }
-
-
-  /**
-   * Build the basic DOM elements and binds the change function
-   */
-  private build() {
-    //
-  }
-
 
   /**
    * This method is called when the window or chart gets resized.
@@ -85,7 +76,6 @@ class BarChart implements IAppView {
    */
   private resize() {
     this.totalWidth = $(this.$node.node()).width();
-
     this.$node.style('width', this.totalWidth);
   }
 
@@ -95,10 +85,13 @@ class BarChart implements IAppView {
    */
   private attachListener() {
     // Call the resize function whenever a resize event occurs
-    //events.on(AppConstants.EVENT_RESIZE, () =>  this.windowResize());
+    events.on(AppConstants.EVENT_RESIZE, () => {
+      this.resize();
+      this.updateItems(this.items);
+    });
 
     events.on(AppConstants.EVENT_DATA_COLLECTION_SELECTED, (evt, items) => {
-      this.$node.selectAll('*').remove(); // remove, because we have completely new items
+      this.items = items;
       this.updateItems(items);
     });
 
@@ -112,129 +105,62 @@ class BarChart implements IAppView {
   }
 
   /**
-   * Create Array with index numbers for the positioning of the bars without the time element
-   * @param items are the elements that are displayed
-   * @param width is the new width of the chart
-   */
-  private generateIndexArray(items, width) {
-    this.items = items;
-
-    this.index = d3.range(1, items.length);
-    const circleScaling = scaleCircles(width, this.items.length);
-
-    for (const i in this.index) {
-      if (this.index.hasOwnProperty(i)) {
-        this.leftValue.push((this.index[i] * circleScaling));
-      }
-    }
-  }
-
-  /**
    * This method updates the chart upon changing the data or if new data arrives.
    * @param items which are used for the chart
    */
   private updateItems(items) {
-    this.items = items;
-    const width = $(window).innerWidth();
-    // Generate the array with positioning numbers
-    this.generateIndexArray(this.items, width);
+    const that = this;
 
-    // Initialize the width
-    this.resize();
-    events.on(AppConstants.EVENT_RESIZE, () => this.windowResize(this.items));
+    // duplicate the first entry to show a bar for the first timestep
+    items.unshift(items[0]);
 
-    let barPromises;
-    const elements = this.$node.selectAll('*');
+    const pairs = d3.pairs(items);
+    const posXScale = getTimeScale(items, this.totalWidth);
 
-    // draw the first bar manually showing only additions
-    const firstSize = this.items[0].item.desc.size;
-    this.drawBars({counts: {a_counts: firstSize[0] * firstSize[1]}, ratios: {a_ratio: 1.0}}, [this.items[0], this.items[0]], 10, this.totalWidth);
+    const $bars = this.$node.selectAll('div.bars')
+      .data(pairs, (d) => d[1].item.desc.id);
 
-    if (elements.empty() === false) {
-      barPromises = this.requestData(this.totalWidth, this.leftValue);
-
-    } else {
-      barPromises = this.requestData(this.totalWidth, this.leftValue);
-      this.leftValue = [];
-    }
-
-    // Check if all bars have been loaded
-    Promise.all(barPromises).then((bars) => {
-      console.log('finished loading of all bars');
-    });
-  }
-
-
-  /**
-   * This event happens upon resizing the window and it calculates the new array
-   * and the index as well as the positioning.
-   * @param items which are displayed in the chart
-   */
-  private windowResize(items) {
-    this.items = items;
-    const width = $(window).innerWidth();
-
-    this.generateIndexArray(this.items, width);
-
-    const elements = d3.selectAll('.bars');
-    if (elements.empty() === false) {
-      this.requestData(width, this.leftValue);
-      elements.remove();
-    }
-    this.leftValue = [];
-  }
-
-  /**
-   * TODO: Documentation
-   * @param totalWidth
-   * @param leftValue
-   * @returns {Promise<TResult>[]}
-   */
-  private requestData(totalWidth, leftValue) {
-    return d3.pairs(this.items)
-      .map((pair) => {
+    $bars.enter().append('div')
+      .classed('bars', true)
+      .style('width', this.widthBarChart + 'px')
+      .style('height', this.heightBarChart + 'px')
+      .on('click', (d) => {
+        selectTimePoint(d[1]);
+      })
+      .each(function(pair, i) {
         const ids = pair.map((d: any) => d.item.desc.id);
-        return Promise.all([BarChart.getJSON(ids), pair, ids])
-          .then((args) => {
-            const json = args[0];
-            const pair = args[1];
-            this.drawBars(json, pair, leftValue.shift(), totalWidth);
+        return BarChart.getJSON(ids)
+          .then((json) => {
+            // shift no-change to additions for first bar
+            if(i === 0) {
+              json.counts.a_counts = json.counts.no_counts;
+              json.counts.no_counts = 0;
+              json.ratios.a_ratio = json.ratios.no_a_ratio;
+              json.ratios.no_a_ratio = 0;
+            }
+
+            that.drawBar(d3.select(this), json, pair);
           });
       });
+
+    $bars.style('left', (d) => posXScale(d[1].time.toDate()) + 'px');
+
+    $bars.exit().remove();
   }
 
   /**
    * This method draws the bars on the timeline or above the timeline.
    * TODO: Documentation
+   * @param $parent
    * @param data
    * @param pair
-   * @param circleScale
-   * @param totalWidth
    */
-  private drawBars(data, pair, circleScale:number, totalWidth:number) {
-    const posXScale = getTimeScale(this.items, totalWidth);
-
+  private drawBar($parent, data, pair) {
     //const barData = this.getBarData(data.ratios, 'ratioName');
     const barData = this.getBarData(data.counts, 'countName');
 
-    const currId = pair.map((d) => d.item.desc.id).join('_');
-    let $barsGroup = this.$node.select(`[data-id="${currId}"]`);
-
-    // create bar group for id if not exists
-    if($barsGroup.node() === null) {
-      $barsGroup = this.$node.append('div')
-        .classed('bars', true)
-        .attr('data-id', currId)
-        .style('width', this.widthBarChart + 'px')
-        .style('height', this.heightBarChart + 'px')
-        .style('left', ((pair[1].time) ? posXScale(pair[1].time.toDate()) : circleScale) + 'px')
-        .on('click', () => {
-          selectTimePoint(pair[1]);
-        });
-    }
-
     //individual bars in the bar group div
-    const $bars = $barsGroup.selectAll('div.bar').data(barData);
+    const $bars = $parent.selectAll('div.bar').data(barData);
     $bars.enter().append('div');
 
     $bars
@@ -256,7 +182,6 @@ class BarChart implements IAppView {
         return 0; // shrink bar to 0 if change is not active
       });
   }
-
 
   /**
    *
