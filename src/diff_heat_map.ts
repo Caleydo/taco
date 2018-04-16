31343da814c6375cc705a77dc66397edcad387a2
Holger Stitz <holger.stitz@jku.at>
2018-04-16 10:54:50 +0200

    Fix tslint with Typescript 2.7.2

--- src/diff_heat_map.ts
+++ src/diff_heat_map.ts
@@ -360,9 +360,7 @@ class DiffHeatMap implements IAppView {
   private render(canvas: HTMLCanvasElement, data: IDiffData) {
     const ctx = canvas.getContext('2d');
 
-    ctx.msImageSmoothingEnabled = false;
-    //if (context.hasOwnProperty('imageSmoothingEnabled')) {
-    (<any>ctx).imageSmoothingEnabled = false;
+    ctx.imageSmoothingEnabled = false;
 
     const totalWidth = AppConstants.HEATMAP_CELL_SIZE * data.union.uc_ids.length * this.scaleFactor.x;
     const totalHeight = AppConstants.HEATMAP_CELL_SIZE * data.union.ur_ids.length * this.scaleFactor.y;
