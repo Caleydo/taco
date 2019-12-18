a14c29950678776af523316b4c397b6dcfbae7ad
Holger Stitz <holger.stitz@datavisyn.io>
2019-12-18 21:21:08 +0100

    Fix tslint

--- src/data_set_selector.ts
+++ src/data_set_selector.ts
@@ -38,6 +38,117 @@ export interface ITacoTimeFormat {
   momentIsSame: moment.unitOfTime.StartOf;
 }
 
+class DataProvider {
+
+  constructor() {
+    //
+  }
+
+  /**
+   * Loads the data and retruns a promise
+   * @returns {Promise<ITacoDataset[]>}
+   */
+  load() {
+    return data
+    //.list((d) => {
+    //  return d.desc.type === 'matrix' && (<IMatrixDataDescription<IValueTypeDesc>>d.desc).value.type === VALUE_TYPE_REAL; // return numerical matrices only
+    //})
+      .list({'type': 'matrix'}) // use server-side filtering
+      .then((list: INumericalMatrix[]) => {
+        const olympicsData: ITacoDataset[] = this.prepareOlympicsData(list);
+        const tcgaData: ITacoDataset[] = this.prepareTCGAData(list);
+        const lastfmData: ITacoDataset[] = this.prepareLastFmData(list);
+        return [].concat(olympicsData, tcgaData, lastfmData);
+      });
+  }
+
+  prepareTCGAData(matrices: INumericalMatrix[]): ITacoDataset[] {
+    const dateRegex = new RegExp(/.*(\d{4})[_-](\d{2})[_-](\d{2}).*/); // matches YYYY_MM_DD or YYYY-MM-DD
+
+    return d3.nest()
+      .key((d: INumericalMatrix) => d.desc.fqname.split('/')[1]).sortKeys(d3.ascending) // e.g. Copynumber, mRNA
+      .key((d: INumericalMatrix) => d.desc.fqname.split('/')[0]).sortKeys(d3.ascending) // e.g., TCGA_GBM_2013_02_22
+      .entries(matrices.filter((d) => dateRegex.test(d.desc.fqname) === true))
+      .map((d: ITacoDataset) => {
+        d.values = d.values.map((e: ITacoTimePoint) => {
+          e.timeFormat = {d3: d3.time.format('%Y-%m-%d'), moment: 'YYYY-MM-DD', momentIsSame: 'day'};
+          e.item = e.values[0]; // shortcut reference
+
+          const matches = e.key.match(dateRegex);
+          e.time = (matches === null) ? null : moment(e.key, AppConstants.PARSE_DATE_FORMATS);
+
+          // NOTE: ids must match with the hard-coded ones in taco_server/src/diff_cache.py -> stratifyMatrix()
+          e.rowStratId = '4CnmfClustering';
+          e.colStratId = 'TreeClusterer1';
+
+          return e;
+        });
+        return d;
+      });
+  }
+
+  prepareOlympicsData(matrices: INumericalMatrix[]): ITacoDataset[] {
+    const olympicsCats = ['total', 'bronze', 'silver', 'gold'];
+    return olympicsCats
+      .map((cat) => {
+        return d3.nest()
+          .key((d: INumericalMatrix) => d.desc.fqname.match(/([A-Z])\w+/g).join(' ')).sortKeys(d3.ascending) // e.g. "Olympic Games Total Medals"
+          .key((d: INumericalMatrix) => d.desc.fqname.match(/(\d)\w+/g).join(' ')).sortKeys(d3.ascending) // e.g. 1920
+          .entries(matrices.filter((d) => d.desc.fqname.toLowerCase().search('olympic') > -1 && d.desc.fqname.toLowerCase().search(cat) > -1));
+      })
+      .reduce((prev, curr) => prev.concat(curr), [])
+      .map((d: ITacoDataset) => {
+        d.values = d.values.map((e: ITacoTimePoint) => {
+          e.timeFormat = {d3: d3.time.format('%Y'), moment: 'YYYY', momentIsSame: 'year'};
+          e.item = e.values[0]; // shortcut reference
+
+          const matches = e.key.match(/(\d)\w+/g);
+          e.time = (matches === null) ? null : moment(e.key, AppConstants.PARSE_DATE_FORMATS);
+
+          e.rowStratId = '';
+          e.colStratId = '';
+
+          return e;
+        });
+        return d;
+      });
+  }
+
+  prepareLastFmData(matrices: INumericalMatrix[]): ITacoDataset[] {
+    const dateRegex = new RegExp(/.*(\d{4}[_-]\d{2}).*/); // matches YYYY_MM or YYYY-MM
+
+    let lastYear = 0;
+    const d3TimeFormat = (d: Date) => {
+      if (d.getFullYear() !== lastYear) {
+        lastYear = d.getFullYear();
+        return d3.time.format('%Y-%m')(d);
+      }
+      return d3.time.format('%m')(d);
+    };
+
+    return d3.nest()
+      .key((d: INumericalMatrix) => 'last.fm').sortKeys(d3.ascending)
+      .key((d: INumericalMatrix) => d.desc.name.match(dateRegex)[1] + '-01').sortKeys(d3.ascending) // e.g. 2010-03
+      .entries(matrices.filter((d) => d.desc.name.toLowerCase().search('last.fm') > -1))
+      .map((d: ITacoDataset) => {
+        d.values = d.values.map((e: ITacoTimePoint) => {
+          e.timeFormat = {d3: d3TimeFormat, moment: 'YYYY-MM', momentIsSame: 'month'};
+          e.item = e.values[0]; // shortcut reference
+
+          const matches = e.key.match(/(\d)\w+/g);
+          e.time = (matches === null) ? null : moment(e.key, AppConstants.PARSE_DATE_FORMATS);
+
+          e.rowStratId = '';
+          e.colStratId = '';
+
+          return e;
+        });
+        return d;
+      });
+  }
+
+}
+
 /**
  * Shows a list of available datasets and lets the user choose one.
  * The selection is broadcasted as event throughout the application.
@@ -178,117 +289,6 @@ class DataSetSelector implements IAppView {
         this.$node.classed('hidden', false);
 
         return this;
-      });
-  }
-
-}
-
-class DataProvider {
-
-  constructor() {
-    //
-  }
-
-  /**
-   * Loads the data and retruns a promise
-   * @returns {Promise<ITacoDataset[]>}
-   */
-  load() {
-    return data
-    //.list((d) => {
-    //  return d.desc.type === 'matrix' && (<IMatrixDataDescription<IValueTypeDesc>>d.desc).value.type === VALUE_TYPE_REAL; // return numerical matrices only
-    //})
-      .list({'type': 'matrix'}) // use server-side filtering
-      .then((list: INumericalMatrix[]) => {
-        const olympicsData: ITacoDataset[] = this.prepareOlympicsData(list);
-        const tcgaData: ITacoDataset[] = this.prepareTCGAData(list);
-        const lastfmData: ITacoDataset[] = this.prepareLastFmData(list);
-        return [].concat(olympicsData, tcgaData, lastfmData);
-      });
-  }
-
-  prepareTCGAData(matrices: INumericalMatrix[]): ITacoDataset[] {
-    const dateRegex = new RegExp(/.*(\d{4})[_-](\d{2})[_-](\d{2}).*/); // matches YYYY_MM_DD or YYYY-MM-DD
-
-    return d3.nest()
-      .key((d: INumericalMatrix) => d.desc.fqname.split('/')[1]).sortKeys(d3.ascending) // e.g. Copynumber, mRNA
-      .key((d: INumericalMatrix) => d.desc.fqname.split('/')[0]).sortKeys(d3.ascending) // e.g., TCGA_GBM_2013_02_22
-      .entries(matrices.filter((d) => dateRegex.test(d.desc.fqname) === true))
-      .map((d: ITacoDataset) => {
-        d.values = d.values.map((e: ITacoTimePoint) => {
-          e.timeFormat = {d3: d3.time.format('%Y-%m-%d'), moment: 'YYYY-MM-DD', momentIsSame: 'day'};
-          e.item = e.values[0]; // shortcut reference
-
-          const matches = e.key.match(dateRegex);
-          e.time = (matches === null) ? null : moment(e.key, AppConstants.PARSE_DATE_FORMATS);
-
-          // NOTE: ids must match with the hard-coded ones in taco_server/src/diff_cache.py -> stratifyMatrix()
-          e.rowStratId = '4CnmfClustering';
-          e.colStratId = 'TreeClusterer1';
-
-          return e;
-        });
-        return d;
-      });
-  }
-
-  prepareOlympicsData(matrices: INumericalMatrix[]): ITacoDataset[] {
-    const olympicsCats = ['total', 'bronze', 'silver', 'gold'];
-    return olympicsCats
-      .map((cat) => {
-        return d3.nest()
-          .key((d: INumericalMatrix) => d.desc.fqname.match(/([A-Z])\w+/g).join(' ')).sortKeys(d3.ascending) // e.g. "Olympic Games Total Medals"
-          .key((d: INumericalMatrix) => d.desc.fqname.match(/(\d)\w+/g).join(' ')).sortKeys(d3.ascending) // e.g. 1920
-          .entries(matrices.filter((d) => d.desc.fqname.toLowerCase().search('olympic') > -1 && d.desc.fqname.toLowerCase().search(cat) > -1));
-      })
-      .reduce((prev, curr) => prev.concat(curr), [])
-      .map((d: ITacoDataset) => {
-        d.values = d.values.map((e: ITacoTimePoint) => {
-          e.timeFormat = {d3: d3.time.format('%Y'), moment: 'YYYY', momentIsSame: 'year'};
-          e.item = e.values[0]; // shortcut reference
-
-          const matches = e.key.match(/(\d)\w+/g);
-          e.time = (matches === null) ? null : moment(e.key, AppConstants.PARSE_DATE_FORMATS);
-
-          e.rowStratId = '';
-          e.colStratId = '';
-
-          return e;
-        });
-        return d;
-      });
-  }
-
-  prepareLastFmData(matrices: INumericalMatrix[]): ITacoDataset[] {
-    const dateRegex = new RegExp(/.*(\d{4}[_-]\d{2}).*/); // matches YYYY_MM or YYYY-MM
-
-    let lastYear = 0;
-    const d3TimeFormat = (d: Date) => {
-      if (d.getFullYear() !== lastYear) {
-        lastYear = d.getFullYear();
-        return d3.time.format('%Y-%m')(d);
-      }
-      return d3.time.format('%m')(d);
-    };
-
-    return d3.nest()
-      .key((d: INumericalMatrix) => 'last.fm').sortKeys(d3.ascending)
-      .key((d: INumericalMatrix) => d.desc.name.match(dateRegex)[1] + '-01').sortKeys(d3.ascending) // e.g. 2010-03
-      .entries(matrices.filter((d) => d.desc.name.toLowerCase().search('last.fm') > -1))
-      .map((d: ITacoDataset) => {
-        d.values = d.values.map((e: ITacoTimePoint) => {
-          e.timeFormat = {d3: d3TimeFormat, moment: 'YYYY-MM', momentIsSame: 'month'};
-          e.item = e.values[0]; // shortcut reference
-
-          const matches = e.key.match(/(\d)\w+/g);
-          e.time = (matches === null) ? null : moment(e.key, AppConstants.PARSE_DATE_FORMATS);
-
-          e.rowStratId = '';
-          e.colStratId = '';
-
-          return e;
-        });
-        return d;
       });
   }
 
