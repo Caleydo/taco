05a58515a61f16e2ae037adf26192db3d870648c
anita-steiner <k9908600@gmx.at>
2020-06-18 10:55:57 +0200

    update config files

--- dist/initialize.js
+++ dist/initialize.js
@@ -1,9 +1,8 @@
 /**
  * Created by Holger Stitz on 25.08.2016.
  */
-import 'file-loader?name=index.html!extract-loader!html-loader!./index.html';
-import 'file-loader?name=404.html!./404.html';
-import 'file-loader?name=robots.txt!./robots.txt';
+import './404.html';
+import './robots.txt';
 import 'phovea_ui/src/_bootstrap';
 import 'phovea_ui/src/_font-awesome';
 import '../scss/main.scss';
--- phovea_registry.js
+++ phovea_registry.js
@@ -4,7 +4,7 @@
  * Licensed under the new BSD license, available at http://caleydo.org/license
  **************************************************************************** */
 
-import {register} from 'phovea_core';
+import {PluginRegistry} from 'phovea_core';
 import reg from './dist/phovea';
 
 /**
@@ -16,4 +16,4 @@ import 'phovea_d3/phovea_registry.js';
 import 'phovea_vis/phovea_registry.js';
 import 'phovea_ui/phovea_registry.js';
 //self
-register('taco',reg);
+PluginRegistry.getInstance().register('taco',reg);
